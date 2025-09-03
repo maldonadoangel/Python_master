3cc3ed1c3132df82b0e345997152f2859148b0d6
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-02 19:46:46 -0600

    Números

---
+++ 06_numeros_funciones.py
@@ -0,0 +1,19 @@
+# importamos el modulo de matematica
+import math
+
+print(round(1.3))
+print(round(1.5))
+
+# Valor absoluto
+print(abs(-55))
+
+
+# buscar el numero más cercano
+print(math.ceil(1.1))
+print(math.floor(1.9999))
+print(math.isnan(23))
+
+# Elevar un número a la potencia
+print(math.pow(10, 2))
+# sacar la raiz de un número
+print(math.sqrt(9))
