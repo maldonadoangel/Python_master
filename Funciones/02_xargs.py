108c1bcc9c06e85b0ed0ea698d12f5d0e2c1f264
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-03 10:47:48 -0600

    Xargs

---
+++ Funciones/02_xargs.py
@@ -0,0 +1,13 @@
+
+# el * ayuda a iterar los argumenos permitiendo agregar n
+# cantidad de argumentos
+def suma(*numeros):
+    resultado = 0
+    for numero in numeros:
+        resultado += numero
+    print(resultado)
+
+
+suma(2, 5, 7)
+suma(2, 6)
+suma(1, 8, 5, 32, 1, 3)
