ba392488bdef18cb82692935123b96049d99afaf
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-02 20:01:38 -0600

    Condicional if

---
+++ Control de flujo/01_comparadores.py
@@ -0,0 +1,6 @@
+
+
+print(f"5 es mayor a 2: {5 > 2}")
+print(f"5 es mayor igual a 2: {5 >= 2}")
+print(f"5 es menor a 2: {5 < 2}")
+print(f"5 es menor o igual a 2: {5 <= 2}")
---
+++ Control de flujo/02_if.py
@@ -0,0 +1,14 @@
+
+
+# Condicional if
+edad = 61
+
+if edad > 60:
+    print("Puede ver la pelicula con descuento de la tercera edad")
+elif edad > 17:
+    print("Puede ver la pelicula")
+else:
+    print("No puede entrar es menor de edad")
+
+
+print("Listo")
