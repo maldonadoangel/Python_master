987e9caf94c0814f1359ce4721c183413db03ceb
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-02 20:33:53 -0600

    Operador ternario

---
+++ Control de flujo/03_operador_ternario.py
@@ -0,0 +1,14 @@
+edad = 61
+
+
+# if edad >= 60:
+#     mensaje = "Descuento de la tercera edad"
+# elif edad >= 18:
+#     mensaje = "Eres mayor de edad"
+# else:
+#     mensaje = "Eres menor de edad"
+
+# Haremos lo mismo pero con un operador ternario
+mensaje = "Es mayor" if edad >= 18 else "Es menor"
+
+print(mensaje)
