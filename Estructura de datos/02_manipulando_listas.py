ef3e0084e1d67d648d73300975defeeb5a28af61
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-09 16:47:04 -0600

    Manipulando listas

---
+++ Estructura de datos/02_manipulando_listas.py
@@ -0,0 +1,12 @@
+mascotas = ["Wolf", "Pelusa", "Pulga", "Copito"]
+print(mascotas[0])
+
+mascotas[0] = "Bicho"
+print(mascotas)
+print(mascotas[:3])
+print(mascotas[2:])
+print(mascotas[1::2])
+
+numeros = list(range(21))
+print(numeros)
+print(numeros[::2])
