1391ee3584d8fae82d9610a4df3a76221f93f623
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-12 19:04:34 -0600

    Buscar elemento

---
+++ Estructura de datos/05_buscar_elemento.py
@@ -0,0 +1,12 @@
+mascotas = ["Pelusa", "Pulga", "Felipe", "Kira", "Wolfgang", "PEPE", "Scooby"]
+
+
+# Verificar si existe un elemento en el arreglo
+if "Wolfgang" in mascotas:
+    print(mascotas.index("Wolfgang"))
+else:
+    print("No se encuentra el elemento")
+
+
+# mostrar cuantas veces existe un elemento en el arreglo
+print(mascotas.count("wolfgang"))
