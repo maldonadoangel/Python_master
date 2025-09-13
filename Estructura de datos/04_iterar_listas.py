07e87d12fe74b383bd736d659079bff32e56211c
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-12 18:47:25 -0600

    Iterar una lista

---
+++ Estructura de datos/04_iterar_listas.py
@@ -0,0 +1,6 @@
+mascotas = ["Pelusa", "Pulga", "Felipe", "Kira", "PEPE", "Scooby"]
+
+
+# para mostrar los indices tenemos que desempaquetar
+for indice, mascota in enumerate(mascotas):
+    print(indice, mascota)
