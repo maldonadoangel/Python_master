ff6a4fc6642cadc7cebf8241ee155461a046b363
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-15 22:46:31 -0600

    Tuplas

---
+++ Estructura de datos/10_tuplas.py
@@ -0,0 +1,11 @@
+numeros = (1, 2, 3, 4) + (5,  6, 7)
+print(numeros)
+
+# Transformar una lista a una tupla
+punto = tuple([1, 2])
+print(punto)
+# para las tuplas no podemos eliminar datos
+
+# para poder modificar una tupla la unica manera es cambiando una tupla por una lista
+lista_numeros = list(numeros)
+print(lista_numeros)
