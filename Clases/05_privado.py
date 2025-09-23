138c333a4c6976ac19f081d3335557dd710c230d
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-23 01:38:19 -0600

    atributos y metodos privados

--- Clases/05_privado.py
+++ Clases/05_privado.py
@@ -3,20 +3,32 @@ class Mascota:
     # Constructor
     def __init__(self, tipoAnimal, nombre):
         self.tipoAnimal = tipoAnimal
-        self.nombre = nombre
+        self.__nombre = nombre
 
     def habla(self):
-        print("Sonido de Animal....")
+        print(f"Sonido de {self.__nombre}")
+
+    def get_nombre(self):
+        return self.__nombre
+    # Si queremos modificar el valor de un atributo privado se utiliza un metodo con el nombre de set
+
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
 
     @classmethod
     def factory(cls):
         return cls("Gato", "Slash")
 
 
-mascota3 = Mascota("Loro", "Botas")
-print(mascota3.habla())
+mascota = Mascota.factory()
 
-mascota = Mascota("Gato", "Kira")
+mascota.habla()
 
-mascota2 = Mascota.factory()
-print(mascota2.tipoAnimal, mascota2.nombre)
+# Con esto podemos ver que no podemos acceder al atributo de nuestro constructor
+# unicamente podemos acceder desde los metodos de la clase
+# print(mascota.__nombre)
+
+
+# La unica manera de poder acceder es atraves de un metodo que creemos para obtener el nombre como utilizar get_nombre
+# Con esto nos aseguramos que se puedan leer el nombre pero no puedan modificar su valor
+print(mascota.get_nombre())
