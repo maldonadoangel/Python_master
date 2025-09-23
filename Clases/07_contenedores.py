43d4c25b22062146077a423eae7b644c3ce42328
Angel Morales Maldonado <maldonadoangel7069@gmail.com>
2025-09-23 05:42:02 -0600

    Contenedores, objetos dentro de otros objetos

---
+++ Clases/07_contenedores.py
@@ -0,0 +1,29 @@
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+    def __str__(self):
+        return f"producto: {self.nombre} - precio: {self.precio}"
+
+
+class Categoria:
+    productos = []
+
+    def __init__(self, nombre, productos):
+        self.nombre = nombre
+        self.productos = productos
+
+    def agregar(self, producto):
+        self.productos.append(producto)
+
+    def imprimir(self):
+        for producto in self.productos:
+            print(producto)
+
+
+kayak = Producto("Kayak", 1000)
+bicicleta = Producto("bicicleta", 600)
+deportes = Categoria("Deportes", [kayak, bicicleta])
+
+deportes.imprimir()
